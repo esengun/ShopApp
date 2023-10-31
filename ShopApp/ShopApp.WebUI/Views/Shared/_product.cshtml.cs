6717b3cd81f324a51d146e25b34af46780a410b9
mustafa esengun <me@kanda.dk>
2023-10-31 18:26:54 +0100

    move common html elements into partial shared views

---
+++ ShopApp/ShopApp.WebUI/Views/Shared/_product.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShopApp.WebUI.Views.Shared
+{
+    public class _productModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
