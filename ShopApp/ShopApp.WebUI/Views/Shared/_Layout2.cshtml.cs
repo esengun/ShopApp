fc3936b5fa37ff48c47c807ead6f68babdc36c36
mustafa esengun <me@kanda.dk>
2023-10-31 19:48:09 +0100

    created a layout html to be used in all pages

---
+++ ShopApp/ShopApp.WebUI/Views/Shared/_Layout2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShopApp.WebUI.Views.Shared
+{
+    public class _Layout2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ShopApp/ShopApp.WebUI/Views/_ViewStart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShopApp.WebUI.Views
+{
+    public class _ViewStartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
