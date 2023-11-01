f7d6c1a07534e8c9093584e8fcbd66254a085b89
mustafa esengun <me@kanda.dk>
2023-11-01 11:13:28 +0100

    added product details page

--- ShopApp/ShopApp.WebUI/Controllers/ProductController.cs
+++ ShopApp/ShopApp.WebUI/Controllers/ProductController.cs
@@ -39,12 +39,7 @@ namespace ShopApp.WebUI.Controllers
         // localhost:5000/product/details/id?
         public IActionResult Details(int id)
         {
-            // name = "Samsung s6"
-            // price = 2000
-            // description = "android"
-
-            var productDetails = new Product { Name = "samsung s6", Price = 2000, Description = "Android" };
-            return View(productDetails);
+            return View(ProductRepository.GetProductById(id));
         }
     }
 }
