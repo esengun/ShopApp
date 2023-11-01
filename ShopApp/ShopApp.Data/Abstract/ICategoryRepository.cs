cadfc3a038bc5eff4e4af1cea3d32fc368fa60b4
mustafa esengun <me@kanda.dk>
2023-11-01 16:22:58 +0100

    created category and product data access interfaces and their implementations

---
+++ ShopApp/ShopApp.Data/Abstract/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using ShopApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Data.Abstract
+{
+	public interface ICategoryRepository
+	{
+		Category GetById(int id);
+		List<Category> GetAll();
+		void Create(Category entity);
+		void Update(Category entity);
+		void Delete(int id);
+	}
+}
