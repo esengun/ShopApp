77fb13e22f26ff7bb8b63f45e42fd2827a304d96
mustafa esengun <me@kanda.dk>
2023-12-02 11:20:33 +0100

    db updated with migrations of user and user role classes

--- ShopApp/ShopApp.WebUI/Identity/ApplicationContext.cs
+++ ShopApp/ShopApp.WebUI/Identity/ApplicationContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ShopApp.WebUI.Identity
 {
-	public class ApplicationContext : IdentityUserContext<User>
+	public class ApplicationContext : IdentityDbContext<User>
 	{
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
         {
